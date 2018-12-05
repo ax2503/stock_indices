ff90218d736a06d04e61fee6057e553c8974c506
Garry Saunders <ax2503@gmail.com>
2018-12-05 12:47:03 +1000

    replaces lists in db

--- loadlists.py
+++ loadlists.py
@@ -8,11 +8,22 @@ def main() :
   text = lists.read()
   tlist = text.split()
   for s in tlist :
-    #db.lrem(s[:-4])
-    print (s[:-4])  
-    #stockcodes = open(s,'r')
-    #stext = stockcodes.read()
-    #slist = stext.split()
+    try :
+      db.lrem(s[:-4])
+    except KeyError :
+      print('Tried removing non-existent list '+ s[:-4])
+
+    #s[:-4] is the filename without the extension  
+    db.lcreate(s[:-4])
+    stockcodes = open(s,'r')
+    stext = stockcodes.read()
+    slist = stext.split()
+    for t in slist :
+      db.ladd(s[:-4], t)
+  
+  db.dump()
+  return
+
 
 
 
